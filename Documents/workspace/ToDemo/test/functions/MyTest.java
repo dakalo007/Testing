0018da7ba23c21faad65668b48aeee9ca6903a6a
dakalo007 <dakalo007>
2012-06-28 16:04:12 +0200

    Including package

---
+++ Documents/workspace/ToDemo/test/functions/MyTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package functions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Dakalo
+ *
+ */
+public class MyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
