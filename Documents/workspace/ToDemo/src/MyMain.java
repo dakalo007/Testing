bc6c065acc9208a13eb6487bf233e0866567477c
dakalo007 <dakalo007>
2012-06-28 15:37:47 +0200

    Everything

--- Documents/workspace/ToDemo/src/MyMain.java
+++ Documents/workspace/ToDemo/src/MyMain.java
@@ -6,7 +6,8 @@
  * @author Dakalo
  *
  */
-public class MyMain {
+public class MyMain 
+{
 
 	/**
 	 * @param args
@@ -14,7 +15,13 @@ public class MyMain {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("This thing is working");
+		System.out.println("This thing is working " + average(71,59));
+	}
+	
+	
+	public int average(int num1,int num2)
+	{
+		return (num1 + num2)/2
 	}
 
 }
